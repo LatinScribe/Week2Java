f32b94535d357fb6226d2b30d2f3977511910f66
Henry "TJ" Chen <127168121+LatinScribe@users.noreply.github.com>
2023-09-12 22:20:04 -0400

    Lecture 2 material

--- src/uoft/csc207/week2/Main.java
+++ src/uoft/csc207/week2/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         String[] name = {"First", "Middle", "Last"};
-	    Person p = new Person(name, "moogah");
+        Person p = new Person(name, "moogah");
+        Person s = new Student(name, "froogle", "number");
         System.out.println(p);
     }
 }
+
+// wifi
+//qq495142
+//ahKi9eegee
